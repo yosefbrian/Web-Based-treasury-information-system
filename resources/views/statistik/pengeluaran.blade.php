9858399260c92996016dc31f36680d1b99ca7d28
yosefbrian <yosefbrian@gmail.com>
2016-05-07 12:42:04 +0700

    ketinggalan

--- resources/views/statistik/pengeluaran.blade.php
+++ resources/views/statistik/pengeluaran.blade.php
@@ -9,7 +9,9 @@
     </div>
 
 </div>
+<div id="pengeluaran-div" align="center" style="width: 1000px; height: 700px"></div>
 
+<?= \Lava::render('BarChart', 'Pengeluaran', 'pengeluaran-div') ?>
 
 
 
