dd6740326414da5f169e0d321cfa1dfb91865bca
yosefbrian <yosefbrian@gmail.com>
2016-05-07 12:41:50 +0700

    Statistik Pengeluaran

--- app/Http/Controllers/statistikController.php
+++ app/Http/Controllers/statistikController.php
@@ -16,34 +16,107 @@ class statistikController extends Controller
 
  public function pengeluaran (){
 
- 	$stocksTable = \Lava::DataTable();
+  $stocksTable = \Lava::DataTable();
 
- $stocksTable->addDateColumn('Day of Month')
-            ->addNumberColumn('Projected')
-            ->addNumberColumn('Official');
+ $stocksTable->addStringColumn('Tahun')
+            ->addNumberColumn('Total');
+
+   
+      $jumlah = array(0,0,0,0,0,0,0,0,0);
+
+$i=0;
+    
+     $year = array('2016%','2017%','2018%','2019%','2020%','2021%','2022%','2023%','2024%','2025%');
 
 
+          for($i=0; $i<=8; $i++){
+
+            $bp2yeah = bp2a::where('created_at', 'LIKE', $year[$i])->get();
+            foreach ($bp2yeah as $bp2) {
+                    $total = $bp2->total;
+
+                    $jumlah[$i]=$jumlah[$i]+(int)$total;
+
+                  
+
+                 }     
 
 
-// Random Data For Example
+            if($jumlah[$i] != 0){
 
-for ($a = 1; $a < 30; $a++)
-{
-    $rowData = array(
-      "2014-8-$a", rand(800,1000), rand(800,1000)
-    );
+              $jml = $jumlah[$i];
 
-    $stocksTable->addRow($rowData);
-}
 
-  $Chart = \Lava::LineChart('Stocks')
+              $thn = substr($year[$i], 0, 4);
+
+              $rowData = array(
+                   $thn, $jml);
+
+                   $stocksTable->addRow($rowData);
+                
+
+            
+
+            }
+
+          }
+         
+          
+
+                
+        
+
+           
+
+         $Chart = \Lava::BarChart('Pengeluaran')
                 ->setOptions(array(
                     'datatable' => $stocksTable,
-                    'title' => 'Stock Market Trends'
+                    'title' => 'Pengeluaran'
                   ));
 
+
+
 return view('statistik.pengeluaran');
+  // return $thn;
+                
  }
-    
+
+
+
+
+//  public function pengeluaran (){
+
+
+
+//   $stocksTable = \Lava::DataTable();
+
+//  $stocksTable->addNumberColumn('Projected')
+//             ->addNumberColumn('Official');
+
+
+
+
+// // Random Data For Example
+
+// for ($a = 1; $a < 30; $a++)
+// {
+//     $rowData = array(
+//       rand(800,1000), rand(800,1000)
+//     );
+
+//     $stocksTable->addRow($rowData);
+// }
+
+//   $Chart = \Lava::BarChart('Stocks')
+//                 ->setOptions(array(
+//                     'datatable' => $stocksTable,
+//                     'title' => 'Stock Market Trends'
+//                   ));
+
+// return view('statistik.pengeluaran');
+//  }
+
+
+
 
 }
--- resources/views/statistik/pengeluaran.blade.php
+++ resources/views/statistik/pengeluaran.blade.php
@@ -1,18 +1,15 @@
 @extends('layouts.app')
-@section('title') analytics:: @parent @stop
 @section('content')
 
 
 
 <div class="row">
     <div class="page-header">
-        <h2>Welcome to analytics</h2>
+        <h2>Pengeluaran</h2>
     </div>
 
 </div>
-<div id="stock-div"></div>
 
-<?= \Lava::render('LineChart', 'Stocks', 'stock-div') ?>
 
 
 
